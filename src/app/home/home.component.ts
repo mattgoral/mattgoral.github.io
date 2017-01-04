b7e029cc56b8dc551e9cce0b0a45fe0257d9d785
mattgoral <m@ttgor.al>
2017-01-04 14:55:17 -0500

    make tabs clickable

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,6 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class HomeComponent implements OnInit {
 	loadAnimation = '';
 
+	tabs = [{
+            title: 'easter eggs!',
+            icon: 'fa-smile-o',
+            url: 'tab-easter-eggs.tpl.html',
+            active: false
+        }, {
+            title: 'about this site',
+            icon: 'fa-info-circle',
+            url: 'tab-about.tpl.html',
+            active: false
+        }, {
+            title: 'welcome',
+            icon: 'fa-star',
+            url: 'tab-welcome.tpl.html',
+            active: true
+    }];
+
+	selectTab(tab) {
+		this.tabs.forEach((tab) => {
+			tab.active = false;
+		});
+		tab.active = true;
+	}
+
   ngOnInit() {
   	this.loadAnimation = 'animated fadeInRight';
   }
