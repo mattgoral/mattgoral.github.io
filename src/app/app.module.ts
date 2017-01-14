1303186ddcddb8ec81e938c8c253391b5a5fb949
mattgoral <m@ttgor.al>
2017-01-13 20:40:31 -0500

    move routes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -13,6 +13,15 @@ import { PageNotFoundComponent } from './page-not-found/page-not-found.component
 import { TabWelcomeComponent } from './tab-welcome/tab-welcome.component';
 import { TabAboutSiteComponent } from './tab-about-site/tab-about-site.component';
 import { TabEasterEggsComponent } from './tab-easter-eggs/tab-easter-eggs.component';
+
+const ROUTES: Routes = [
+    { path: '', component: HomeComponent },
+    { path: 'home', component: HomeComponent },
+    { path: 'work', component: WorkComponent },
+    { path: 'about', component: AboutComponent },
+    { path: 'contact', component: ContactComponent },
+    { path: '**', component: PageNotFoundComponent }
+  ];
 
 @NgModule({
   declarations: [
@@ -30,14 +39,7 @@ import { TabEasterEggsComponent } from './tab-easter-eggs/tab-easter-eggs.compon
     BrowserModule,
     FormsModule,
     HttpModule,
-    RouterModule.forRoot([
-      { path: '', component: HomeComponent },
-      { path: 'home', component: HomeComponent },
-      { path: 'work', component: WorkComponent },
-      { path: 'about', component: AboutComponent },
-      { path: 'contact', component: ContactComponent },
-      { path: '**', component: PageNotFoundComponent }
-    ])
+    RouterModule.forRoot(ROUTES)
   ],
   providers: [],
   bootstrap: [AppComponent]
